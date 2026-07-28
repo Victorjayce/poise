41569bdae1f8492293977ce9ce218bd2a7c326d2
Victor Jayce <nickchives45@gmail.com>
2026-07-28 11:15:01 +0100

    updated taskcard to use gesture detector in expanded

--- lib/widgets/taskcard.dart
+++ lib/widgets/taskcard.dart
@@ -24,166 +24,180 @@ class _TaskCardState extends State<TaskCard> {
   Widget build(BuildContext context) {
     if (widget.task != null) {
       final task = widget.task!;
-      return Container(
-        margin: const EdgeInsets.only(bottom: 10),
-        padding: const EdgeInsets.all(8),
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            expanded = !expanded;
+          });
+        },
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: const EdgeInsets.all(8),
 
-        decoration: BoxDecoration(
-          color: Color(0xff001F5A),
-          borderRadius: BorderRadius.circular(6),
-        ),
+          decoration: BoxDecoration(
+            color: Color(0xff001F5A),
+            borderRadius: BorderRadius.circular(6),
+          ),
 
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Checkbox(
-              value: task.isCompleted,
-              fillColor: WidgetStatePropertyAll(widget.color),
-              side: BorderSide.none,
-              // once checked, don't allow unchecking
-              onChanged: task.isCompleted
-                  ? null
-                  : (value) {
-                      if (value == true) {
-                        check(context, task);
-                      }
-                    },
-            ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Checkbox(
+                value: task.isCompleted,
+                fillColor: WidgetStatePropertyAll(widget.color),
+                side: BorderSide.none,
+                // once checked, don't allow unchecking
+                onChanged: task.isCompleted
+                    ? null
+                    : (value) {
+                        if (value == true) {
+                          check(context, task);
+                        }
+                      },
+              ),
 
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          task.activeTask,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            task.activeTask,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
 
-                      if (task.description.isNotEmpty)
-                        IconButton(
-                          icon: Icon(
-                            expanded
-                                ? Icons.keyboard_arrow_up
-                                : Icons.keyboard_arrow_down,
-                            color: widget.color,
+                        if (task.description.isNotEmpty)
+                          IconButton(
+                            icon: Icon(
+                              expanded
+                                  ? Icons.keyboard_arrow_up
+                                  : Icons.keyboard_arrow_down,
+                              color: widget.color,
+                            ),
+
+                            onPressed: () {
+                              setState(() {
+                                expanded = !expanded;
+                              });
+                            },
                           ),
-
-                          onPressed: () {
-                            setState(() {
-                              expanded = !expanded;
-                            });
-                          },
-                        ),
-                    ],
-                  ),
-
-                  AnimatedCrossFade(
-                    firstChild: const SizedBox.shrink(),
-
-                    secondChild: Text(
-                      task.description,
-                      style: TextStyle(color: Colors.white),
+                      ],
                     ),
 
-                    crossFadeState: expanded
-                        ? CrossFadeState.showSecond
-                        : CrossFadeState.showFirst,
+                    AnimatedCrossFade(
+                      firstChild: const SizedBox.shrink(),
 
-                    duration: const Duration(milliseconds: 200),
-                  ),
-                ],
+                      secondChild: Text(
+                        task.description,
+                        style: TextStyle(color: Colors.white),
+                      ),
+
+                      crossFadeState: expanded
+                          ? CrossFadeState.showSecond
+                          : CrossFadeState.showFirst,
+
+                      duration: const Duration(milliseconds: 200),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
     } else {
       final task = widget.lvltask!;
-      return Container(
-        margin: const EdgeInsets.only(bottom: 10),
-        padding: const EdgeInsets.all(8),
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            expanded = !expanded;
+          });
+        },
+        child: Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: const EdgeInsets.all(8),
 
-        decoration: BoxDecoration(
-          color: Color(0xff001F5A),
-          borderRadius: BorderRadius.circular(6),
-        ),
+          decoration: BoxDecoration(
+            color: Color(0xff001F5A),
+            borderRadius: BorderRadius.circular(6),
+          ),
 
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Checkbox(
-              value: !task.isActive,
-              fillColor: WidgetStatePropertyAll(widget.color),
-              side: BorderSide.none,
-              // once checked, don't allow unchecking
-              onChanged: !task.isActive
-                  ? null
-                  : (value) {
-                      if (value == true) {
-                        checklevel(context, task);
-                      }
-                    },
-            ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Checkbox(
+                value: !task.isActive,
+                fillColor: WidgetStatePropertyAll(widget.color),
+                side: BorderSide.none,
+                // once checked, don't allow unchecking
+                onChanged: !task.isActive
+                    ? null
+                    : (value) {
+                        if (value == true) {
+                          checklevel(context, task);
+                        }
+                      },
+              ),
 
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          task.name,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            task.name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
-                      ),
 
-                      if (task.description.isNotEmpty)
-                        IconButton(
-                          icon: Icon(
-                            expanded
-                                ? Icons.keyboard_arrow_up
-                                : Icons.keyboard_arrow_down,
-                            color: widget.color,
+                        if (task.description.isNotEmpty)
+                          IconButton(
+                            icon: Icon(
+                              expanded
+                                  ? Icons.keyboard_arrow_up
+                                  : Icons.keyboard_arrow_down,
+                              color: widget.color,
+                            ),
+
+                            onPressed: () {
+                              setState(() {
+                                expanded = !expanded;
+                              });
+                            },
                           ),
-
-                          onPressed: () {
-                            setState(() {
-                              expanded = !expanded;
-                            });
-                          },
-                        ),
-                    ],
-                  ),
-
-                  AnimatedCrossFade(
-                    firstChild: const SizedBox.shrink(),
-
-                    secondChild: Text(
-                      task.description,
-                      style: TextStyle(color: Colors.white),
+                      ],
                     ),
 
-                    crossFadeState: expanded
-                        ? CrossFadeState.showSecond
-                        : CrossFadeState.showFirst,
+                    AnimatedCrossFade(
+                      firstChild: const SizedBox.shrink(),
 
-                    duration: const Duration(milliseconds: 200),
-                  ),
-                ],
+                      secondChild: Text(
+                        task.description,
+                        style: TextStyle(color: Colors.white),
+                      ),
+
+                      crossFadeState: expanded
+                          ? CrossFadeState.showSecond
+                          : CrossFadeState.showFirst,
+
+                      duration: const Duration(milliseconds: 200),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       );
     }
